96ffed2569f18bc27c6858f226a94db3219e0b02
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-08 19:54:05 +0200

    minor change

--- base/settings.py
+++ base/settings.py
@@ -7,7 +7,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 #endregion DEPLOYMENT SETTINGS
 
