cfdb2dbd563b63ef82c0e033719978047c1bc221
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-10 10:35:13 +0200

    Finished login views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def my_login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('blog:post_list')
+    
+    context = {
+        'loginform': form
+    }
+
+    return render(request,'blog/my-login.html')
